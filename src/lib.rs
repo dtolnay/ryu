ee8f8fd5e90eaea690d751295d4fde7cde12c267
David Tolnay <dtolnay@gmail.com>
2018-08-07 11:57:21 -0700

    Release 0.2.1

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@
 //! ```
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/ryu/0.2.0")]
+#![doc(html_root_url = "https://docs.rs/ryu/0.2.1")]
 #![cfg_attr(feature = "no-panic", feature(use_extern_macros))]
 #![cfg_attr(
     feature = "cargo-clippy",
