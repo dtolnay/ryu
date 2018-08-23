2916d64226ca31f8da09ba79e9b54a1b7269150a
David Tolnay <dtolnay@gmail.com>
2018-08-23 18:23:54 -0400

    Release 0.2.5

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@
 //! ```
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/ryu/0.2.4")]
+#![doc(html_root_url = "https://docs.rs/ryu/0.2.5")]
 #![cfg_attr(
     feature = "cargo-clippy",
     allow(
