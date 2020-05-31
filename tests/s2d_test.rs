5b4e48f6da74a0a4f1f07768bc4590da32afe6a6
David Tolnay <dtolnay@gmail.com>
2020-05-30 20:15:39 -0700

    Require d2s full tables for s2d

--- tests/s2d_test.rs
+++ tests/s2d_test.rs
@@ -18,6 +18,7 @@
 // is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 // KIND, either express or implied.
 
+#![cfg(not(feature = "small"))]
 #![allow(dead_code)]
 
 #[path = "../src/common.rs"]
