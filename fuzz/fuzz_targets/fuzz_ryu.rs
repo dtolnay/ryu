49896436fd3965f11bc7bbd65cab9d374c9fb4f5
David Tolnay <dtolnay@gmail.com>
2022-05-15 14:22:52 -0700

    Assert size of string in fuzz target

--- fuzz/fuzz_targets/fuzz_ryu.rs
+++ fuzz/fuzz_targets/fuzz_ryu.rs
@@ -2,6 +2,7 @@
 
 use arbitrary::Arbitrary;
 use libfuzzer_sys::fuzz_target;
+use std::mem;
 
 #[derive(Arbitrary, Debug)]
 enum FloatInput {
@@ -12,10 +13,11 @@ enum FloatInput {
 fuzz_target!(|inputs: (FloatInput, bool)| {
     let (input, finite) = inputs;
     let mut buffer = ryu::Buffer::new();
-    match (input, finite) {
+    let string = match (input, finite) {
         (FloatInput::F32(val), false) => buffer.format(val),
         (FloatInput::F32(val), true) => buffer.format_finite(val),
         (FloatInput::F64(val), false) => buffer.format(val),
         (FloatInput::F64(val), true) => buffer.format_finite(val),
     };
+    assert!(string.len() <= mem::size_of::<ryu::Buffer>());
 });
