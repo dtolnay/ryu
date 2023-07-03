4dfbb7f59f90d9284dd3ce0ad6f656b92872b4c3
David Tolnay <dtolnay@gmail.com>
2023-07-03 11:29:27 -0700

    Release 1.0.14

--- src/lib.rs
+++ src/lib.rs
@@ -81,7 +81,7 @@
 //! notation.
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/ryu/1.0.13")]
+#![doc(html_root_url = "https://docs.rs/ryu/1.0.14")]
 #![allow(
     clippy::cast_lossless,
     clippy::cast_possible_truncation,
