522321fa0095fae43e78f7aca7bd1679ddf293a4
David Tolnay <dtolnay@gmail.com>
2023-09-13 22:45:23 -0600

    Update name of clippy clone impl lint
        warning: lint `clippy::incorrect_clone_impl_on_copy_type` has been renamed to `clippy::non_canonical_clone_impl`
          --> src/buffer/mod.rs:86:13
           |
        86 |     #[allow(clippy::incorrect_clone_impl_on_copy_type)] // false positive https://github.com/rust-lang/rust-clippy/issues/11072
           |             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: use the new name: `clippy::non_canonical_clone_impl`
           |
           = note: `#[warn(renamed_and_removed_lints)]` on by default

--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -83,7 +83,7 @@ impl Copy for Buffer {}
 
 impl Clone for Buffer {
     #[inline]
-    #[allow(clippy::incorrect_clone_impl_on_copy_type)] // false positive https://github.com/rust-lang/rust-clippy/issues/11072
+    #[allow(clippy::non_canonical_clone_impl)] // false positive https://github.com/rust-lang/rust-clippy/issues/11072
     fn clone(&self) -> Self {
         Buffer::new()
     }
