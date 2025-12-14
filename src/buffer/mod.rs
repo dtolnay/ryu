2c0c4897d9bbd42077e3f9eeb96eb5fcdad25cee
David Tolnay <dtolnay@gmail.com>
2025-12-13 20:18:24 -0800

    Resolve ptr_as_ptr pedantic clippy lint
        warning: `as` casting between raw pointers without changing their constness
          --> src/buffer/mod.rs:74:43
           |
        74 |             let n = f.write_to_ryu_buffer(self.bytes.as_mut_ptr() as *mut u8);
           |                                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: try `pointer::cast`, a safer alternative: `self.bytes.as_mut_ptr().cast::<u8>()`
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#ptr_as_ptr
           = note: `-W clippy::ptr-as-ptr` implied by `-W clippy::pedantic`
           = help: to override `-W clippy::pedantic` add `#[allow(clippy::ptr_as_ptr)]`
    
        warning: `as` casting between raw pointers without changing their constness
          --> src/buffer/mod.rs:76:47
           |
        76 |             let slice = slice::from_raw_parts(self.bytes.as_ptr() as *const u8, n);
           |                                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: try `pointer::cast`, a safer alternative: `self.bytes.as_ptr().cast::<u8>()`
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#ptr_as_ptr

--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -71,9 +71,9 @@ impl Buffer {
     #[cfg_attr(feature = "no-panic", no_panic)]
     pub fn format_finite<F: Float>(&mut self, f: F) -> &str {
         unsafe {
-            let n = f.write_to_ryu_buffer(self.bytes.as_mut_ptr() as *mut u8);
+            let n = f.write_to_ryu_buffer(self.bytes.as_mut_ptr().cast::<u8>());
             debug_assert!(n <= self.bytes.len());
-            let slice = slice::from_raw_parts(self.bytes.as_ptr() as *const u8, n);
+            let slice = slice::from_raw_parts(self.bytes.as_ptr().cast::<u8>(), n);
             str::from_utf8_unchecked(slice)
         }
     }
